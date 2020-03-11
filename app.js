be9163b6b4d9eea55ce3b54663f5b4cce0fc7284
Adrian Rutkiewicz <adrian.rutkiewicz@artifexmundi.com>
2020-03-11 18:10:05 +0100

    research #1

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const cmd = require('node-cmd');
+const exec = require('child_process').exec;
 
-cmd.get(
-    'adb shell "ps | grep balefire"',
-   (err, data, stderr) => {
-    //    response = data.split(' ');
-       console.log(data);
-   }
-);
+const getProcessId = () =>
+{
+    cmd.get(
+        'adb shell "ps | grep balefire"',
+       (err, data, stderr) => {
+        const pidStart = data.search(/\d{4,5}/);
+        const response = parseInt(data.slice(pidStart, pidStart + 5));
+       }
+    );
+}
+
+function runCmd(command)
+{
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) console.log(error);
+            resolve(stdout ? stdout : stderr);
+        });
+    });
+}
+
+async function printDevices()
+{
+    const cmdOut = await runCmd('adb devices');
+    console.log(cmdOut);
+}
+
+getProcessId();
+
+printDevices();
