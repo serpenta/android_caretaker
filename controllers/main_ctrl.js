c793aac3e6a02a156c35255e26bf3d72847f9917
serpenta <ignisserpenta@gmail.com>
2020-04-23 12:24:24 +0200

    increase buffer size wo 1024**3

--- controllers/main_ctrl.js
+++ controllers/main_ctrl.js
@@ -1,10 +1,10 @@
-const exec = require('child_process').exec;
+const childProcess = require('child_process');
 const path = require('path');
 
 function runCmd(command)
 {
     return new Promise((resolve, reject) => {
-        exec(command, (error, stdout, stderr) => {
+        childProcess.exec(command, { maxBuffer: 1024**3 }, (error, stdout, stderr) => {
             if (error) console.warn(error);
             resolve(stdout ? stdout : stderr);
         });
@@ -15,32 +15,39 @@ async function printDevices()
 {
     runCmd('adb devices')
     .then(value => console.log(value));
-    // console.log(cmdOut);
 }
 
 async function deleteApp()
 {
     runCmd('adb uninstall com.artifexmundi.balefire')
-    .then(value => console.log(value));
+    .then(value => console.log(value));;
 }
 
 async function pushObb(filepath, obbFilename)
 {
     runCmd(`adb push ${filepath+obbFilename} mnt/sdcard/Android/obb/com.artifexmundi.balefire/${obbFilename}`)
-    .then(value => console.log(value));
+    .then(value => console.log(value));;
 }
 
 async function installApp(filepath, apkFilename, obbFilename)
 {
+    console.log(`[installApp]: installing... ${apkFilename}`);
     await runCmd(`adb install ${filepath+apkFilename}`)
-    .then(value => console.log(value));
+    .then(value => console.log(value));;
 
+    console.log(`[installApp]: creating obb directory...`);
     await runCmd(`adb shell "mkdir mnt/sdcard/Android/obb/com.artifexmundi.balefire`)
-    .then(value => console.log(value));
+    .then(value => console.log(value));;
 
-    await pushObb(filepath, obbFilename);
+    console.log(`[installApp]: pushing... ${obbFilename}`);
+    await pushObb(filepath, obbFilename)
+    .then(value => console.log(value));;
+
+    console.log(`[installApp]: app installed!`);
 }
 
 printDevices();
 
-pushObb("C:\\Users\\Serpenta\\Downloads\\baldbound\\", "main.12098.com.artifexmundi.balefire.obb");
+// deleteApp();
+
+installApp("C:\\Users\\Serpenta\\Downloads\\baldbound\\", "Bladebound_ftr-cached_dependencies(12098)-Debug.apk", "main.12098.com.artifexmundi.balefire.obb");
