9289991411a5f9fea5379182269818947520747f
serpenta <ignisserpenta@gmail.com>
2020-04-23 11:47:23 +0200

    add commands for deleting and installing app

--- controllers/main_ctrl.js
+++ controllers/main_ctrl.js
@@ -1,4 +1,5 @@
 const exec = require('child_process').exec;
+const path = require('path');
 
 function runCmd(command)
 {
@@ -17,4 +18,29 @@ async function printDevices()
     // console.log(cmdOut);
 }
 
+async function deleteApp()
+{
+    runCmd('adb uninstall com.artifexmundi.balefire')
+    .then(value => console.log(value));
+}
+
+async function pushObb(filepath, obbFilename)
+{
+    runCmd(`adb push ${filepath+obbFilename} mnt/sdcard/Android/obb/com.artifexmundi.balefire/${obbFilename}`)
+    .then(value => console.log(value));
+}
+
+async function installApp(filepath, apkFilename, obbFilename)
+{
+    await runCmd(`adb install ${filepath+apkFilename}`)
+    .then(value => console.log(value));
+
+    await runCmd(`adb shell "mkdir mnt/sdcard/Android/obb/com.artifexmundi.balefire`)
+    .then(value => console.log(value));
+
+    await pushObb(filepath, obbFilename);
+}
+
 printDevices();
+
+pushObb("C:\\Users\\Serpenta\\Downloads\\baldbound\\", "main.12098.com.artifexmundi.balefire.obb");
