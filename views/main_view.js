c4ac180ec5cb347ca4933e7b748187cd5168d512
Adrian Rutkiewicz <ignisserpenta@gmail.com>
2020-11-05 00:43:41 +0100

    create common with utility functions

--- views/main_view.js
+++ views/main_view.js
@@ -1,36 +1,14 @@
 const { ipcRenderer } = require('electron');
 
-const getRadio = radioName =>
-{
-    const radio = document.getElementsByName(radioName);
-    let value = null;
-    radio.forEach(el => 
-        {
-            if (el.checked)
-            {
-                value = el.value;
-            } 
-        });
-    return value;
-}
-
-const getCheckbox = boxName =>
-{
-    return document.getElementById(boxName).checked;
-}
-
-const getInput = input =>
-{
-    return document.getElementById(input).value;
-}
+const utils = require('../common/utilities');
 
 document
     .getElementById('install-app')
     .addEventListener('click', () => {
         console.log('ipcRenderer: install-app');
         ipcRenderer.send('btn-install-app',
-            getInput('device-id'),
-            getInput('file-abs-path'),
-            getInput('apk-filename'),
-            getInput('obb-filename'));
+            utils.getInput('device-id'),
+            utils.getInput('file-abs-path'),
+            utils.getInput('apk-filename'),
+            utils.getInput('obb-filename'));
     });
