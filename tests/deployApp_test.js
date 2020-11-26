22f7fd7dda41c42898ef42d41eb800ea2ad5a101
Adrian Rutkiewicz <ignisserpenta@gmail.com>
2020-11-26 07:33:43 +0100

    update test

--- tests/deployApp_test.js
+++ tests/deployApp_test.js
@@ -16,8 +16,5 @@ deployApp(
     deviceID= "",
     filepath= "I:\\bladebound\\",
     packageName= "com.artifexmundi.balefire",
-    apkFilename= "Bladebound_rel-2-10-0(12768)-Release.apk",
-    obbFilename= "main.12768.com.artifexmundi.balefire.obb");
-
-// FFY5T17C21001655 - Huawei
-// bc5afd11 - Xiaomi
+    apkFilename= "Bladebound_rel-2-11-0(12865)-Release.apk",
+    obbFilename= "main.12865.com.artifexmundi.balefire.obb");
